3acbfef54726f2156bb2f5c697749c154fd70c1f
manish404 <57221671+manish404@users.noreply.github.com>
2020-07-17 13:50:51 +0545

    Add files via upload

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 import sys
+import socket
 import http.server
 import socketserver
+import os
 
 PORT = 8080
+IP = socket.gethostbyname(socket.gethostname()) 
 
 if len(sys.argv) == 2:
     PORT = int(sys.argv[1])
@@ -10,7 +13,9 @@ if len(sys.argv) == 2:
 if __name__ == "__main__":
     handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(("", PORT), handler)
-    print(f"Serving on port : {PORT}")
+    print(f">> Serving on port : {PORT}")
+    print(f">> Shared folder '{os.getcwd()}'")
+    print(f">> Enter {IP}:{PORT} on any device's browser to access this folder!")
     httpd.serve_forever()
 
 # pyinstaller --onefile --icon=youricon.ico filename.py
